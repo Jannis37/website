bb8f9da20625413bf1ca73a782e4fd0575e36421
Jannis Weichler <jw@addix.net>
2023-01-27 11:18:13 +0100

    installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+	"./src/**/*.{html,ts}",
+],
+  theme: {
+    extend: {
+      colors: {
+        addix: '#4195c4'
+      },
+    },
+  },
+  plugins: [],
+}
